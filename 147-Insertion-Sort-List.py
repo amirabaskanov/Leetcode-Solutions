1956837ea35f249bf110c664ebdec3bad8c09978
Amir Abaskanov <51660433+amirabaskanov@users.noreply.github.com>
2026-09-17 16:41:10 -0700

    Create 147-Insertion-Sort-List.py

---
+++ 147-Insertion-Sort-List.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev, cur = head, head.next
+
+        while cur:
+            if cur.val >= prev.val:
+                prev, cur = cur, cur.next
+                continue
+
+            key = dummy
+            while cur.val > key.next.val:
+                key = key.next
+
+            prev.next = cur.next
+            cur.next = key.next
+            key.next = cur
+            cur = prev.next
+        
+        return dummy.next
